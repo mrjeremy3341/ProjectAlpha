9a7e1ec8de394a21de1de39d33cf8f2f41cd9abe
ReformedMrozu <56279952+ReformedMrozu@users.noreply.github.com>
2020-06-01 23:44:38 +0200

    basic camera controll

---
+++ Assets/Scripts/CamerControll.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CamerControll : MonoBehaviour
+{
+    public Transform player;
+    public float cameraLevel = -10.00f; // -10 default
+    // public float xOffset;
+    // public float yOffset;
+    // public float zOffset;
+ 
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x, player.position.y, cameraLevel);  //every frame moves camera to match player position
+    }
+}
