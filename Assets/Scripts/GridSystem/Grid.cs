2d11ad724b709de5a4eef1afd13b169dac7ba50c
mrjeremy3341 <33188901+mrjeremy3341@users.noreply.github.com>
2020-06-01 17:48:44 -0400

    Implements Bade Grid Class and Tester object

---
+++ Assets/Scripts/GridSystem/Grid.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid
+{
+    private int width;
+    private int height;
+    private float cellSize;
+    private int[,] gridArray;
+    
+    public Grid(int width, int height, float cellSize)
+    {
+        this.width = width;
+        this.height = height;
+        this.cellSize = cellSize;
+
+        gridArray = new int[width, height];
+
+        for(int x = 0; x < gridArray.GetLength(0); x++)
+        {
+            for (int y = 0; y < gridArray.GetLength(0); y++)
+            {
+                Debug.Log(x + ", " + y);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x, y + 1), Color.white, 100f);
+                Debug.DrawLine(GetWorldPosition(x, y), GetWorldPosition(x + 1, y), Color.white, 100f);
+            }
+        }
+        Debug.DrawLine(GetWorldPosition(0, height), GetWorldPosition(width, height), Color.white, 100f);
+        Debug.DrawLine(GetWorldPosition(width, 0), GetWorldPosition(width, height), Color.white, 100f);
+    }
+
+    private Vector2 GetWorldPosition(int x, int y)
+    {
+        return new Vector2(x, y) * cellSize;
+    }
+}
---
+++ Assets/Scripts/GridSystem/GridTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridTest : MonoBehaviour
+{
+    private void Start()
+    {
+        Grid grid = new Grid(5, 5, 1f);
+    }
+}
